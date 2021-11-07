8dc09899d61e2d41d062bccea8552cd36e8aa0aa
sergii.volodko <svolodko@8thlight.com>
2021-11-07 23:41:30 +0000

    Extract performance total calculation variables

--- python/statement.py
+++ python/statement.py
@@ -1,38 +1,56 @@
 import math
 
 
+def format_as_dollars(amount):
+    return f"${amount:0,.2f}"
+
 def statement(invoice, plays):
     total_amount = 0
     volume_credits = 0
+
     result = f'Statement for {invoice["customer"]}\n'
 
-    def format_as_dollars(amount):
-        return f"${amount:0,.2f}"
+    for performance in invoice['performances']:
+        if not performance['playID'] in plays:
+            continue
+        play = plays[performance['playID']]
 
-    for perf in invoice['performances']:
-        play = plays[perf['playID']]
+        tragedy_minimum_amount = 40000
+        tragedy_additional_seat_price = 1000
+        tragedy_seats_limit = 30
+
+        comedy_minimum_amount = 30000
+        comedy_additional_seat_price = 500
+        comedy_extra_seats_one_time_fee = 10000
+        comedy_seats_limit = 20
+        comedy_regular_seat_price = 300
+        performance_total = 0
+
         if play['type'] == "tragedy":
-            this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
+            performance_total = tragedy_minimum_amount
+            if performance['audience'] > tragedy_seats_limit:
+                additional_seats = performance['audience'] - tragedy_seats_limit
+                performance_total += tragedy_additional_seat_price * additional_seats
         elif play['type'] == "comedy":
-            this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
+            performance_total = comedy_minimum_amount
+            if performance['audience'] > comedy_seats_limit:
+                performance_total += comedy_extra_seats_one_time_fee
+                additional_seats = performance['audience'] - comedy_seats_limit
+                performance_total += comedy_additional_seat_price * additional_seats
 
-            this_amount += 300 * perf['audience']
+            performance_total += comedy_regular_seat_price * performance['audience']
 
         else:
             raise ValueError(f'unknown type: {play["type"]}')
 
         # add volume credits
-        volume_credits += max(perf['audience'] - 30, 0)
+        volume_credits += max(performance['audience'] - 30, 0)
         # add extra credit for every ten comedy attendees
         if "comedy" == play["type"]:
-            volume_credits += math.floor(perf['audience'] / 5)
+            volume_credits += math.floor(performance['audience'] / 5)
         # print line for this order
-        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
-        total_amount += this_amount
+        result += f' {play["name"]}: {format_as_dollars(performance_total/100)} ({performance["audience"]} seats)\n'
+        total_amount += performance_total
 
     result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
     result += f'You earned {volume_credits} credits\n'
