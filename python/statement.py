0cce2bdde9b56ce50926a4c0e4266f8961dc480e
sergii.volodko <svolodko@8thlight.com>
2021-11-08 00:16:35 +0000

    Clarify and improve calculate_volume_credits

--- python/statement.py
+++ python/statement.py
@@ -1,8 +1,13 @@
 import math
 
 
+PLAY_TYPE_COMEDY = "comedy"
+PLAY_TYPE_TRAGEDY = "tragedy"
+
+
 def format_as_dollars(amount):
     return f"${amount:0,.2f}"
+
 
 def calculate_performance_total_amount(play_type: str, occupied_seats_count: int):
     tragedy_minimum_amount = 400
@@ -17,12 +22,12 @@ def calculate_performance_total_amount(play_type: str, occupied_seats_count: int
 
     performance_total = 0
 
-    if play_type == "tragedy":
+    if play_type == PLAY_TYPE_TRAGEDY:
         performance_total = tragedy_minimum_amount
         if occupied_seats_count > tragedy_seats_limit:
             additional_seats_count = occupied_seats_count - tragedy_seats_limit
             performance_total += tragedy_additional_seat_price * additional_seats_count
-    elif play_type == "comedy":
+    elif play_type == PLAY_TYPE_COMEDY:
         performance_total = comedy_minimum_amount
         if occupied_seats_count > comedy_seats_limit:
             performance_total += comedy_extra_seats_one_time_fee
@@ -34,12 +39,20 @@ def calculate_performance_total_amount(play_type: str, occupied_seats_count: int
         raise ValueError(f'unknown type: {play_type}')
     return performance_total
 
+
 def calculate_volume_credits(play_type: str, occupied_seats_count: int):
-    volume_credits = max(occupied_seats_count - 30, 0)
-    # add extra credit for every ten comedy attendees
-    if play_type == "comedy":
-        volume_credits += math.floor(occupied_seats_count / 5)
+    volume_credits = 0
+
+    regular_seats_capacity = 30
+    additional_seats = occupied_seats_count - regular_seats_capacity
+    if additional_seats > 0:
+        volume_credits += additional_seats
+
+    if play_type == PLAY_TYPE_COMEDY:
+        seats_group_size = 5
+        volume_credits += math.floor(occupied_seats_count / seats_group_size)
     return volume_credits
+
 
 def statement(invoice, plays):
     total_amount = 0
@@ -53,15 +66,14 @@ def statement(invoice, plays):
         play = plays[performance['playID']]
         play_type = play['type']
         occupied_seats_count = performance['audience']
+
         performance_total = calculate_performance_total_amount(play_type, occupied_seats_count)
         total_volume_credits += calculate_volume_credits(play_type, occupied_seats_count)
+        total_amount += performance_total
 
         # print line for this order
         result += f' {play["name"]}: {format_as_dollars(performance_total)} ({occupied_seats_count} seats)\n'
-        total_amount += performance_total
 
     result += f'Amount owed is {format_as_dollars(total_amount)}\n'
     result += f'You earned {total_volume_credits} credits\n'
     return result
-
-
